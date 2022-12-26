f5b881d61a4e5241c1cd95771c00b2a8ef2e808c
BrandonHeatSo <stardustgrave100msn@ybb.ne.jp>
2022-12-26 15:08:12 +0000

    【E10.5】追加機能10～15分単位へ切替え～後の微修正（真のラスト！！！）

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -9,25 +9,6 @@ module AttendancesHelper
     # どれにも当てはまらなかった場合はfalseを返します。
     return false
   end
-  
-  def time_ceil_quarter(time)
-    min = time.min
-    
-    case min
-    when 1..15
-      q = 15
-    when 16..30
-      q = 30
-    when 31..45
-      q = 45
-    when 46..59
-      q = 60
-    else
-      q = 0
-    end
-    
-    time += (q-min).minutes
-  end
 
   # 出勤時間と退勤時間を受け取り、在社時間を計算して返します。
   def working_times(start, finish)
