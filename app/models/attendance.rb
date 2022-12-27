0719d31d2ecb2f78f658f2333ee627942da72170
BrandonHeatSo <stardustgrave100msn@ybb.ne.jp>
2022-12-27 13:19:34 +0000

    【E10.5.5】未来ＮＧのバリデーション設定、リベンジ（これで本当にラスト）！！

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -6,10 +6,14 @@ class Attendance < ApplicationRecord
 
   # 出勤時間が存在しない場合、退勤時間は無効
   validate :finished_at_is_invalid_without_a_started_at
-  # 退勤時間が存在しない場合、出勤時間は無効（勤怠編集画面での更新時のみ）
+  # 退勤時間が存在しない場合、退勤時間は無効（勤怠編集画面での更新時のみ）
   validate :started_at_is_invalid_without_a_finished_at, on: :update_one_month_edit
   # 出勤・退勤時間どちらも存在する時、出勤時間より早い退勤時間は無効
   validate :started_at_than_finished_at_fast_if_invalid
+  # 未来の出勤時間は無効
+  validate :started_at_cannot_be_in_the_future, on: :update_one_month_edit
+  # 未来の退勤時刻は無効
+  validate :finished_at_cannot_be_in_the_future, on: :update_one_month_edit
   
   def finished_at_is_invalid_without_a_started_at
     errors.add(:started_at, "が必要です") if started_at.blank? && finished_at.present?
@@ -23,5 +27,13 @@ class Attendance < ApplicationRecord
     if started_at.present? && finished_at.present?
       errors.add(:started_at, "より早い退勤時間は無効です") if started_at > finished_at
     end
+  end
+  
+  def started_at_cannot_be_in_the_future
+    errors.add(:started_at, "は未来の情報だと無効です") if started_at.present? && Date.current < worked_on
+  end
+  
+  def finished_at_cannot_be_in_the_future
+    errors.add(:finished_at, "は未来の情報だと無効です") if finished_at.present? && Date.current < worked_on
   end
 end
