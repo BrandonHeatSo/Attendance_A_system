2434a915f082b4c1b6cd8c87a602432ac448a0e5
Brandon Heat <stardustgrave100msn@ybb.ne.jp>
2023-02-05 00:27:22 +0900

    【F7.1】ＣＳＶフォーマット向け必要５カラムの新追加『のみ』

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20230203101023) do
+ActiveRecord::Schema.define(version: 20230204150930) do
 
   create_table "attendances", force: :cascade do |t|
     t.date "worked_on"
@@ -40,9 +40,14 @@ ActiveRecord::Schema.define(version: 20230203101023) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "affiliation"
-    t.datetime "basic_time", default: "2023-02-02 23:00:00"
-    t.datetime "work_time", default: "2023-02-02 22:30:00"
+    t.datetime "basic_time", default: "2023-02-04 23:00:00"
+    t.datetime "work_time", default: "2023-02-04 22:30:00"
     t.integer "employee_number"
+    t.string "uid"
+    t.datetime "basic_work_time", default: "2023-02-04 23:00:00"
+    t.datetime "designated_work_start_time", default: "2023-02-05 00:00:00"
+    t.datetime "designated_work_end_time", default: "2023-02-05 09:00:00"
+    t.boolean "superior", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
